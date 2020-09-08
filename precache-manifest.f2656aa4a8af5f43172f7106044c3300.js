f8299f4163188074ea90f00aba02710510a8060c
chojs23 <chojs990222@gmail.com>
2020-09-08 21:47:52 +0900

    Updates

---
+++ precache-manifest.f2656aa4a8af5f43172f7106044c3300.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "198c5cefd68c811e0d7af6285dc76b26",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "58fb8e5ff9487adf865f",
+    "url": "/movie-app/static/css/main.524005e5.chunk.css"
+  },
+  {
+    "revision": "1c1867ee385e1fd15da6",
+    "url": "/movie-app/static/js/2.3a05f077.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app/static/js/2.3a05f077.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58fb8e5ff9487adf865f",
+    "url": "/movie-app/static/js/main.ce84ff0f.chunk.js"
+  },
+  {
+    "revision": "8c0fe60c15a349e095c4",
+    "url": "/movie-app/static/js/runtime-main.1fbae34b.js"
+  }
+]);
